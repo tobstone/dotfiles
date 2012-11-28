1bbfe7af1208e691bae20b961a42b3fb8b4c8770
Tobias Steinhoff <tobias@stonehope.de>
2012-11-28 18:09:21 +0100

    add newsbeuter config

--- setup.bash
+++ setup.bash
@@ -4,12 +4,19 @@ git pull
 git submodule init
 git submodule update
 
+mkdir -p ~/.newsbeuter
+
 function undlos() {
 for file in *
 do
   if   [ "${file}" == "bin"  ]
   then 
     cd ; rm -Rf bin; ln -s -v $DOTFILES/$file $file
+    continue
+  fi
+  if   [ "${file}" == "newsbeuter"  ]
+  then 
+    cd ; rm -f .newsbeuter/config; ln -s -v $DOTFILES/$file/config .$file/config
     continue
   fi
   if [ "${file}" == "ReadMe.md" ] ||  [ "${file}" == "setup.bash" ] || [ "${file}" == "License" ]
